ebcf66df639654c4ff87bf89f373023d6b37e299
Billett Development <kbillettdev@hotmail.com>
2020-02-14 18:36:05 +0000

    Updates

---
+++ precache-manifest.9f884fc158bdc359f79a6fd8326cb59d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79994e3b91d6361c3d71fb904a17e2c9",
+    "url": "/Monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a576af9ad407eefd40bd",
+    "url": "/Monsters-rolodex/static/css/main.2d022d27.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/Monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a576af9ad407eefd40bd",
+    "url": "/Monsters-rolodex/static/js/main.aafb35cb.chunk.js"
+  },
+  {
+    "revision": "f50133f2c4b44c266753",
+    "url": "/Monsters-rolodex/static/js/runtime-main.da3dab2d.js"
+  }
+]);
